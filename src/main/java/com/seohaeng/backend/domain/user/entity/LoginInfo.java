bb4f204a512b23f193bfff0c84728e39859a0988
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-07-02 03:59:15 +0900

    ♻️ [Refactor] LoginInfo 엔티티 리팩토링

--- src/main/java/com/seohaeng/backend/domain/user/entity/LoginInfo.java
+++ src/main/java/com/seohaeng/backend/domain/user/entity/LoginInfo.java
@@ -2,19 +2,31 @@ package com.seohaeng.backend.domain.user.entity;
 
 import com.seohaeng.backend.domain.common.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class LoginInfo extends BaseEntity {
-    @Id @GeneratedValue
+
+    @Id
+    @GeneratedValue
     private Long id;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private String provider;
 
-    @Column(nullable = false)
+    @Column(length = 255)
     private String email;
+
+    @Column(length = 12, unique = true)
+    @Size(min=8, max=12)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
