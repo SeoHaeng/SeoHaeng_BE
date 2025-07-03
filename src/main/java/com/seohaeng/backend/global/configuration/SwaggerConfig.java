6d7df1babc62870daf732bce20809dd7c83bcd50
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-07-04 02:44:18 +0900

    ✨ [Feat] Swagger 추가

---
+++ src/main/java/com/seohaeng/backend/global/configuration/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.seohaeng.backend.global.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI SeohaengAPI() {
+        Info info = new Info()
+                .title("서행(書行):책 타고 떠나는 느린 강원도 여행")
+                .description("서행 API 명세서")
+                .version("1.0.0");
+
+        String jwtSchemeName = "JWT TOKEN";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
