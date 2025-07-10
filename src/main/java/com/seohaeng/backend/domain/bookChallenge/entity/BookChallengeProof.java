77e7ba5e14fce0a0e2b9de9a742a95e73e787115
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-07-10 18:34:50 +0900

    ♻️[Refactor] BookChallengeProof 엔티티 필드 추가

--- src/main/java/com/seohaeng/backend/domain/bookChallenge/entity/BookChallengeProof.java
+++ src/main/java/com/seohaeng/backend/domain/bookChallenge/entity/BookChallengeProof.java
@@ -1,6 +1,7 @@
 package com.seohaeng.backend.domain.bookChallenge.entity;
 
 import com.seohaeng.backend.domain.common.BaseEntity;
+import com.seohaeng.backend.domain.place.entity.Place;
 import com.seohaeng.backend.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
@@ -19,19 +20,47 @@ public class BookChallengeProof extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 2048)
-    private String bookChallengeProofContent;
-
-    @Column(nullable = false)
-    private Integer bookChallengeProofLikes = 0;
-
-    @OneToMany(mappedBy = "bookChallengeProof", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<BookChallengrProofImage> BookChallengrProofImageList = new ArrayList<>();
-
-    @OneToMany(mappedBy = "bookChallengeProof", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<BookChallengeProofComment> bookChallengeProofComment = new ArrayList<>();
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @Column(nullable = false, length = 2048)
+    private String presentMessage;
+
+    @Column(nullable = false, length = 2048)
+    private String bookChallengeProofContent;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Integer bookChallengeProofLikes = 0;
+
+    @Column(nullable = false, length = 30)
+    private String receivedBookTitle;
+
+    @Column(nullable = false, length = 30)
+    private String receivedBookAuthor;
+
+    @Column(nullable = false, length = 300)
+    private String receivedBookImage;
+
+    @Column(nullable = false, length = 30)
+    private String givenBookTitle;
+
+    @Column(nullable = false, length = 30)
+    private String givenBookAuthor;
+
+    @Column(nullable = false, length = 300)
+    private String givenBookImage;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id", nullable = false)
+    private Place place;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "bookChallengeProof", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BookChallengeProofImage> bookChallengeProofImageList = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "bookChallengeProof", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BookChallengeProofComment> bookChallengeProofComment = new ArrayList<>();
 }
