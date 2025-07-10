a03af8ff59e870b707579a3ed7677b8dbd88eb4f
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-07-10 18:42:06 +0900

    🚚[Rename] BookChallengeProofImage으로 파일명 변경

---
+++ src/main/java/com/seohaeng/backend/domain/bookChallenge/entity/BookChallengeProofImage.java
@@ -0,0 +1,24 @@
+package com.seohaeng.backend.domain.bookChallenge.entity;
+
+import com.seohaeng.backend.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class BookChallengeProofImage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 2048)
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_challenge_proof_id", nullable = false)
+    private BookChallengeProof bookChallengeProof;
+}
