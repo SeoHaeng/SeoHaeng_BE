dd2e1f959f78b34c53b00624562647bf037e67f9
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-08-18 02:59:12 +0900

    ♻️[Refactor] User 엔티티 cascade 및 orphanRemoval 설정 추가

--- src/main/java/com/seohaeng/backend/domain/user/entity/User.java
+++ src/main/java/com/seohaeng/backend/domain/user/entity/User.java
@@ -1,7 +1,12 @@
 package com.seohaeng.backend.domain.user.entity;
 
 import com.seohaeng.backend.domain.bookChallenge.entity.BookChallenge;
+import com.seohaeng.backend.domain.bookChallenge.entity.BookChallengeProof;
+import com.seohaeng.backend.domain.bookChallenge.entity.BookChallengeProofComment;
 import com.seohaeng.backend.domain.common.entity.BaseEntity;
+import com.seohaeng.backend.domain.readingSpot.entity.ReadingSpot;
+import com.seohaeng.backend.domain.readingSpot.entity.ReadingSpotComment;
+import com.seohaeng.backend.domain.readingSpot.entity.ReadingSpotLike;
 import com.seohaeng.backend.domain.readingSpot.entity.ReadingSpotScrap;
 import com.seohaeng.backend.domain.travelCourse.entity.Stamp;
 import com.seohaeng.backend.domain.travelCourse.entity.TravelCourse;
@@ -31,18 +36,31 @@ public class User extends BaseEntity {
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private LoginInfo loginInfo;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    private List<BookChallenge> bookChallengeBooks = new ArrayList<>();
+    // 독립 서점 사장 여부
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Owner owner;
 
+    // 북 챌린지
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    private List<Stamp> stampList = new ArrayList<>();
-
+    private List<BookChallenge> bookChallenges = new ArrayList<>();
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    private List<TravelCourse> travelCourses = new ArrayList<>();
+    private List<BookChallengeProof> bookChallengeProofList = new ArrayList<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<BookChallengeProofComment> bookChallengeProofCommentList = new ArrayList<>();
 
+    // 공간 책갈피
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ReadingSpot> readingSpotList = new ArrayList<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ReadingSpotLike> readingSpotLikeList = new ArrayList<>();
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<ReadingSpotScrap> readingSpotScrapList = new ArrayList<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ReadingSpotComment> readingSpotCommentList = new ArrayList<>();
 
-    @OneToOne(mappedBy = "user")
-    private Owner owner; // 독립 서점 사장 여부
+    // 여행 일정
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Stamp> stampList = new ArrayList<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<TravelCourse> travelCourses = new ArrayList<>();
 }
