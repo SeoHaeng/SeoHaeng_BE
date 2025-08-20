c64fdfa604c5a758993cc6e022a59591e68afa75
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-08-21 05:38:15 +0900

    ✨[Feat] TourApiClient 구성

---
+++ src/main/java/com/seohaeng/backend/domain/place/dto/tourapiResponseDTO/DetailCommonResponseDTO.java
@@ -0,0 +1,73 @@
+package com.seohaeng.backend.domain.place.dto.tourapiResponseDTO;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+public class DetailCommonResponseDTO {
+    private Response response;
+
+    @Getter
+    @Setter
+    public static class Response {
+        private Header header;
+        private Body body;
+    }
+
+    @Getter
+    @Setter
+    public static class Header {
+        private String resultCode;
+        private String resultMsg;
+    }
+
+    @Getter
+    @Setter
+    public static class Body {
+        private Items items;
+        private Integer numOfRows;
+        private Integer pageNo;
+        private Integer totalCount;
+    }
+
+    @Getter
+    @Setter
+    public static class Items {
+        private List<CommonItem> item;
+    }
+
+    @Getter
+    @Setter
+    public static class CommonItem {
+        private String contentid;
+        private String contenttypeid;
+        private String title;
+        private String createdtime;
+        private String modifiedtime;
+        private String tel;
+        private String telname;
+        private String homepage;
+        private String firstimage;
+        private String firstimage2;
+        private String cpyrhtDivCd;
+        private String areacode;
+        private String sigungucode;
+        private String lDongRegnCd;
+        private String lDongSignguCd;
+        private String lclsSystm1;
+        private String lclsSystm2;
+        private String lclsSystm3;
+        private String cat1;
+        private String cat2;
+        private String cat3;
+        private String addr1;
+        private String addr2;
+        private String zipcode;
+        private String mapx;
+        private String mapy;
+        private String mlevel;
+        private String overview;
+    }
+}
