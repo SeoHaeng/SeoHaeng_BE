56a4ce4cc2cb4128c0b332733b1ee2c167632ace
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-08-31 14:05:00 +0900

    ♻️[Refactor] User 삭제 시 외래키 제약 조건 문제 수정

--- src/main/java/com/seohaeng/backend/domain/user/entity/User.java
+++ src/main/java/com/seohaeng/backend/domain/user/entity/User.java
@@ -3,11 +3,15 @@ package com.seohaeng.backend.domain.user.entity;
 import com.seohaeng.backend.domain.bookChallenge.entity.BookChallenge;
 import com.seohaeng.backend.domain.bookChallenge.entity.BookChallengeProof;
 import com.seohaeng.backend.domain.bookChallenge.entity.BookChallengeProofComment;
+import com.seohaeng.backend.domain.bookChallenge.entity.BookChallengeProofLike;
 import com.seohaeng.backend.domain.common.entity.BaseEntity;
+import com.seohaeng.backend.domain.notification.entity.Notification;
+import com.seohaeng.backend.domain.place.entity.place.SavedPlace;
 import com.seohaeng.backend.domain.readingSpot.entity.ReadingSpot;
 import com.seohaeng.backend.domain.readingSpot.entity.ReadingSpotComment;
 import com.seohaeng.backend.domain.readingSpot.entity.ReadingSpotLike;
 import com.seohaeng.backend.domain.readingSpot.entity.ReadingSpotScrap;
+import com.seohaeng.backend.domain.review.entity.Review;
 import com.seohaeng.backend.domain.travelCourse.entity.Stamp;
 import com.seohaeng.backend.domain.travelCourse.entity.TravelCourse;
 import jakarta.persistence.*;
@@ -66,4 +70,20 @@ public class User extends BaseEntity {
     private List<Stamp> stampList = new ArrayList<>();
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<TravelCourse> travelCourses = new ArrayList<>();
+
+    // 장소 저장
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<SavedPlace> savedPlaceList = new ArrayList<>();
+
+    // 리뷰
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Review> reviewList = new ArrayList<>();
+
+    // 알림
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Notification> notificationList = new ArrayList<>();
+
+    // 북 챌린지 인증 좋아요
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<BookChallengeProofLike> bookChallengeProofLikeList = new ArrayList<>();
 }
